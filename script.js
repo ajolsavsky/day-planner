51002f32f72811d044ffe7b17327acde99795aca
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-05-06 13:49:20 -0500

    Updated with working background colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var currentDate = moment().format("MMM Do, YYYY");
-var currentHour = moment().format("hh a");
+var currentHour = moment().format("h a");
 $("#currentDay").text(currentDate);
 
 var timeblocksEl = $(".timeblocks");
@@ -8,27 +8,52 @@ var timeblocksEl = $(".timeblocks");
 
 //Dynamically render scheduler
 function renderTimes() {
+    // Array of times
     var time = [
-        moment("09:00:00", "hh:mm:ss").format("h a"),
-        moment("10:00:00", "hh:mm:ss").format("h a"),
-        moment("11:00:00", "hh:mm:ss").format("h a"),
-        moment("12:00:00", "hh:mm:ss").format("h a"),
-        moment("13:00:00", "hh:mm:ss").format("h a"),
-        moment("14:00:00", "hh:mm:ss").format("h a"),
-        moment("15:00:00", "hh:mm:ss").format("h a"),
-        moment("16:00:00", "hh:mm:ss").format("h a"),
-        moment("17:00:00", "hh:mm:ss").format("h a"),
+        moment("09:00", "hh.mm").format("hh a"),
+        moment("10:00", "hh:mm").format("hh a"),
+        moment("11:00", "hh:mm").format("hh a"),
+        moment("12:00", "hh:mm").format("hh a"),
+        moment("13:00", "hh:mm").format("hh a"),
+        moment("14:00", "hh:mm").format("hh a"),
+        moment("15:00", "hh:mm").format("hh a"),
+        moment("16:00", "hh:mm").format("hh a"),
+        moment("17:00", "hh:mm").format("hh a"),
     ];
-
+    // Loop that dyamically renders times to the page
     for (var i = 0; i < time.length; i++) {
-        var containerDiv = $('<div>').addClass('input-group mb-3');
+        var containerDiv = $('<div>').addClass('input-group pr-4 pl-4');
         var timeSpan = $('<span>').addClass('input-group-text');
         var plannerInput = $('<input type="text">').addClass('form-control');
         var saveButton = $('<button type="button">').addClass('btn btn-outline-secondary').text('Save');
+
+        var currentScheduleIndex = time[i];
+        //Convert array string to a moment
+        var scheduleMoment = moment(currentScheduleIndex, 'h a')
+        //Calculate difference in hours between current time and the schedule Index time
+        var timeDifference = moment(currentScheduleIndex, 'h a').diff(moment(), 'hours', true);
+        console.log(timeDifference);
+
+        //Changes input background based on if the time has passed, is in the future, or is the current time
+        if (timeDifference < -1) {
+            console.log ("This hour has already happened");
+            plannerInput.addClass('bg-secondary text-light');
+        } else if (timeDifference > 0) {
+            console.log ("This hour is in the future");
+            plannerInput.addClass('bg-success text-light');
+        } else {
+            console.log ("This is the current time");
+            plannerInput.addClass('bg-danger text-light');
+
+        }
         
+        //Append the container div to the timeblocks div
         timeblocksEl.append(containerDiv);
 
+        //Define the text for the timespan element
         timeSpan.text(time[i]);
+
+        //Append individual time, input field, save button to the page
         containerDiv.append(
             timeSpan,
             plannerInput,
@@ -39,10 +64,5 @@ function renderTimes() {
 
 renderTimes();
 
-if (currentHour === "11 pm") {
-    console.log("It's the same")
-} else if (currentHour) {
-    console.log("It's before")
-} else {
-    console.log("It's after")
-}
+
+//TODO: Event propogation to the timeblocks div for buttons/text input
