14bb2d344a58acffe9eec342e8d9a55e43aad427
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-05-06 00:25:47 -0500

    Updated with moment times

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var currentDate = moment().format("MMM Do, YYYY");
+var currentHour = moment().format("hh a");
 $("#currentDay").text(currentDate);
 
 var timeblocksEl = $(".timeblocks");
@@ -8,15 +9,15 @@ var timeblocksEl = $(".timeblocks");
 //Dynamically render scheduler
 function renderTimes() {
     var time = [
-        '9AM',
-        '10AM',
-        '11AM',
-        '12PM',
-        '1PM',
-        '2PM',
-        '3PM',
-        '4PM',
-        '5PM',
+        moment("09:00:00", "hh:mm:ss").format("h a"),
+        moment("10:00:00", "hh:mm:ss").format("h a"),
+        moment("11:00:00", "hh:mm:ss").format("h a"),
+        moment("12:00:00", "hh:mm:ss").format("h a"),
+        moment("13:00:00", "hh:mm:ss").format("h a"),
+        moment("14:00:00", "hh:mm:ss").format("h a"),
+        moment("15:00:00", "hh:mm:ss").format("h a"),
+        moment("16:00:00", "hh:mm:ss").format("h a"),
+        moment("17:00:00", "hh:mm:ss").format("h a"),
     ];
 
     for (var i = 0; i < time.length; i++) {
@@ -37,3 +38,11 @@ function renderTimes() {
 }
 
 renderTimes();
+
+if (currentHour === "11 pm") {
+    console.log("It's the same")
+} else if (currentHour) {
+    console.log("It's before")
+} else {
+    console.log("It's after")
+}
